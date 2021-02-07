be8e7cb294af8852411ddc7ede8406fb939f10e5
SpoonsTheUtensil <alexschmierbach@yahoo.com>
2021-02-06 18:11:05 -0600

    Created Main and Reading from Input File and parsing string correctly for infixToPostFix()

--- src/infixExpressionParser/InfixExpressionParser.java
+++ src/infixExpressionParser/InfixExpressionParser.java
@@ -1,57 +1,115 @@
 package infixExpressionParser;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class InfixExpressionParser {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		File f = new File("input.txt");
+		while (!f.exists()) {
+			System.out.println("File does not exist!");
+			System.out.println("...Exiting Program");
+			System.exit(0);
+		}
+		
+		FileInputStream inputFile = new FileInputStream(f);
+		Scanner fileReader = new Scanner(inputFile);
+		String evaluationFinal = "";
+		
+		while (fileReader.hasNext()) {
+			String evaluation = fileReader.nextLine().replace(" ", "");
+			char[]evalLetters = evaluation.toCharArray();
+			String tempOperator = "";
+			int count = 0;
+			
+			for (@SuppressWarnings("unused") char letter : evalLetters){
+				char current = evalLetters[count];
+				evaluationFinal += " " + current;
+				count++;
+				
+				if (count >= evalLetters.length) {
+					break;
+				}
+				char next = evalLetters[count];
 
+				if (((current == '>' && next == '=')) || 
+				    	((current == '<' && next == '=')) || 
+				    		((current == '=' && next == '=')) || 
+				    			((current == '!' && next == '=')) || 
+				    				((current == '&' && next == '&'))|| 
+				    					((current == '|' && next == '|'))) {
+						
+				    tempOperator += Character.toString(next);
+				    evaluationFinal += tempOperator;
+				    tempOperator = "";
+				    count++;
+				}
+			}
+			System.out.println(infixToPostFix(evaluationFinal));
+			evaluationFinal = "";
+		}
+		fileReader.close();
 	}
+	
+	
+		//System.out.println(infixToPostFix(evaluationFinal));
 	
 	public static int precedence(String operator) {
 		if (operator.equals("^")) { return 7; }
-		if (operator.equals("*") || operator.equals("/")) { return 6; }
+		if (operator.equals("*") || operator.equals("/") || operator.equals("%")) { return 6; }
 		if (operator.equals("+") || operator.equals("-")) { return 5; }
 		if (operator.equals(">") || operator.equals(">=") || operator.equals("<") || operator.equals("<=")) { return 4; }
 		if (operator.equals("==") || operator.equals("!=")) { return 3; }
 		if (operator.equals("&&")) { return 2; }
 		if (operator.equals("||")) { return 1; }
 		throw new IllegalArgumentException(String.format("Operator %s is not supported.", operator));
-	} 
+	}
 	
-	public static String infixToPostfix(String infixExp) {
+	public static String infixToPostFix(String infixExp) {
 		Scanner scanner = new Scanner(infixExp);
 		Stack<String> stack = new Stack<>();
 		StringBuilder postfixExp = new StringBuilder();
 		while (scanner.hasNext()) {
-		String token = scanner.next();
-		// If the current token is an operand, append it to the postfix expression string.
-		if (Character.isDigit(token.charAt(0))) { postfixExp.append(token).append(' '); }
-		// If the current token is opening parenthesis, push it onto the stack.
-		else if (token.equals("(")) { stack.push(token); }
-		else if (token.equals(")")) {
-		// Pop all operators from the stack, until an opening parenthesis is seen on top of the stack.
-		// Append each popped operator to the postfix expression string.
-		while (!stack.peek().equals("(")) { postfixExp.append(stack.pop()).append(' '); }
-		// Pop the opening parenthesis from the stack.
-		stack.pop();
-		} else {
-		// The current token is an operator.
-		// Keep popping operators (and append them to postfix expression string) from the stack, until:
-		// 1) the stack is empty,
-		// 2) an opening parenthesis is seen on top of the stack, or
-		// 3) the current operator has higher precedence than the operator on top of the stack.
-		while (!stack.isEmpty() && !stack.peek().equals("(") &&
-		precedence(token) <= precedence(stack.peek())) {
-		postfixExp.append(stack.pop()).append(' ');
-		}
-		// Push the current operator onto the stack.
-		stack.push(token);
-		}
+			String token = scanner.next();
+			// If the current token is an operand, append it to the postfix expression string.
+			if (Character.isDigit(token.charAt(0))) { 
+				postfixExp.append(token);
+			}
+			// If the current token is opening parenthesis, push it onto the stack.
+			else if (token.equals("(")) {
+				stack.push(token);
+			}
+			else if (token.equals(")")) {
+				// Pop all operators from the stack, until an opening parenthesis is seen on top of the stack.
+				// Append each popped operator to the postfix expression string.
+				while (!stack.peek().equals("(")) {
+					postfixExp.append(stack.pop());
+				}
+				// Pop the opening parenthesis from the stack.
+				stack.pop();
+			} else {
+				// The current token is an operator.
+				// Keep popping operators (and append them to postfix expression string) from the stack, until:
+				// 1) the stack is empty,
+				// 2) an opening parenthesis is seen on top of the stack, or
+				// 3) the current operator has higher precedence than the operator on top of the stack.
+				while (!stack.isEmpty() && !stack.peek().equals("(") && precedence(token) <= precedence(stack.peek())) {
+					postfixExp.append(stack.pop());
+				}
+				// Push the current operator onto the stack.
+				stack.push(token);
+			}
 		}
 		// Pop the remaining operators from the stack and append them to the postfix expression string.
-		while (!stack.isEmpty()) { postfixExp.append(stack.pop()).append(' '); }
+		while (!stack.isEmpty()) {
+			postfixExp.append(stack.pop());
+		}
 		scanner.close();
 		return postfixExp.toString();
-		} 
-	
+	}
 }
