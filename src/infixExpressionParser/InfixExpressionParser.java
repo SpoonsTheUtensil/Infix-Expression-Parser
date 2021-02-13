6737e3450952916840372d7cdaa7fd8cb122efbb
Abbas Dargahi <adargahi97@gmail.com>
2021-02-13 13:28:35 -0600

    Add / 0 and clean up

--- src/infixExpressionParser/InfixExpressionParser.java
+++ src/infixExpressionParser/InfixExpressionParser.java
@@ -59,8 +59,6 @@ public class InfixExpressionParser {
 	}
 	
 	
-		//System.out.println(infixToPostFix(evaluationFinal));
-	
 	//Method to establish precedence of an operator
 	public static int precedence(String operator) {
 		if (operator.equals("^")) { return 7; }
@@ -80,7 +78,7 @@ public class InfixExpressionParser {
 		StringBuilder postfixExp = new StringBuilder();
 		while (scanner.hasNext()) {
 			String token = scanner.next();
-			// If the current token is an operand, append it to the postfix expression string.
+			// If the current token is an operand, append it to the postfix expressionion string.
 			if (Character.isDigit(token.charAt(0))) { 
 				postfixExp.append(token);
 			}
@@ -90,7 +88,7 @@ public class InfixExpressionParser {
 			}
 			else if (token.equals(")")) {
 				// Pop all operators from the stack, until an opening parenthesis is seen on top of the stack.
-				// Append each popped operator to the postfix expression string.
+				// Append each popped operator to the postfix expressionion string.
 				while (!stack.peek().equals("(")) {
 					postfixExp.append(stack.pop());
 				}
@@ -98,7 +96,7 @@ public class InfixExpressionParser {
 				stack.pop();
 			} else {
 				// The current token is an operator.
-				// Keep popping operators (and append them to postfix expression string) from the stack, until:
+				// Keep popping operators (and append them to postfix expressionion string) from the stack, until:
 				// 1) the stack is empty,
 				// 2) an opening parenthesis is seen on top of the stack, or
 				// 3) the current operator has higher precedence than the operator on top of the stack.
@@ -109,7 +107,7 @@ public class InfixExpressionParser {
 				stack.push(token);
 			}
 		}
-		// Pop the remaining operators from the stack and append them to the postfix expression string.
+		// Pop the remaining operators from the stack and append them to the postfix expressionion string.
 		while (!stack.isEmpty()) {
 			postfixExp.append(stack.pop());
 		}
@@ -117,11 +115,11 @@ public class InfixExpressionParser {
 		return postfixExp.toString();
 	}
 	
-	public static int evalPostfix(String express) { 
+	public static int evalPostfix(String expression) { 
 		Stack<Integer> st = new Stack<>(); 
 	          
-	    for(int i=0; i < express.length(); i++) { 
-	    	char ch = express.charAt(i); 
+	    for(int i=0; i < expression.length(); i++) { 
+	    	char ch = expression.charAt(i); 
 	             
 	        if(Character.isDigit(ch))
 	        	st.push(ch - '0'); 
@@ -132,32 +130,37 @@ public class InfixExpressionParser {
 	                  
 	            switch(ch) { 
 	            	case '+': 
-	                st.push(value2 + value1); 
-	                break; 
+	            		st.push(value2 + value1); 
+	            		break; 
 	                      
 	                case '-': 
-	                st.push(value2 - value1); 
-	                break; 
+	                	st.push(value2 - value1); 
+	                	break; 
 	                        
 	                case '*': 
-	                st.push(value2*value1); 
-	                break;
+	                	st.push(value2*value1); 
+	                	break;
 	                
 	                case '/': 
-	                st.push(value2/value1); 
-	                break;
+	                	try {
+			                st.push(value2 / value1); 
+	                	}
+	                	catch (ArithmeticException e) {
+	                		System.out.println("Can't be divided by zero");
+	                	}
+	                	break;
 	                
 	                case '^': 
 	                	st.push((int) Math.pow(value2, value1)); 
 		                break;
 		            
-	                case '%': 
-		                st.push(value2%value1); 
+	                case '%':
+			            st.push(value2%value1); 
 		                break;
 	                
 	                case '>':
 	                	//Find if its >= or not and make sure its not at the end of the string
-	                	if (i != express.length() - 1 && express.charAt(i+1) == '=') {
+	                	if (i != expression.length() - 1 && expression.charAt(i+1) == '=') {
 	                		if (value2 >= value1) { st.push(1); }
 	                		else { st.push(0); }
 	                		//Move i forward one to pass the '='
@@ -171,7 +174,7 @@ public class InfixExpressionParser {
 	                
 	                case '<':
 	                	//Find if its <= or not and make sure its not at the end of the string
-	                	if (i != express.length() - 1 && express.charAt(i+1) == '=') {
+	                	if (i != expression.length() - 1 && expression.charAt(i+1) == '=') {
 	                		if (value2 <= value1) { st.push(1); }
 	                		
 	                		else { st.push(0); }
@@ -186,8 +189,7 @@ public class InfixExpressionParser {
 	                	break;
 	                	
 	                case '!':
-	                	
-	                	if (i != express.length() - 1 && express.charAt(i+1) == '=') {
+	                	if (i != expression.length() - 1 && expression.charAt(i+1) == '=') {
 	                		if (value2 != value1) { st.push(1); }
 	                		
 	                		else { st.push(0); }
@@ -197,7 +199,7 @@ public class InfixExpressionParser {
 	                	break;
 	                
 	                case '=':
-	                	if (i != express.length() - 1 && express.charAt(i+1) == '=') {
+	                	if (i != expression.length() - 1 && expression.charAt(i+1) == '=') {
 	                		if (value2 == value1) { st.push(1); }
 	                		
 	                		else { st.push(0); }
@@ -207,7 +209,7 @@ public class InfixExpressionParser {
 	                	break;
 	                
 	                case '&':
-	                	if (i != express.length() - 1 && express.charAt(i+1) == '&') {
+	                	if (i != expression.length() - 1 && expression.charAt(i+1) == '&') {
 	                		if (value2 == 1 && value1 == 1) { st.push(1); }
 	                		
 	                		else { st.push(0); }
@@ -216,9 +218,8 @@ public class InfixExpressionParser {
 	                	}
 	                	break;
 	                	
-	                	
 	                case '|':
-	                	if (i != express.length() - 1 && express.charAt(i+1) == '|') {
+	                	if (i != expression.length() - 1 && expression.charAt(i+1) == '|') {
 	                		if (value2 == 1 && value1 == 1 || 
 	                			value2 == 1 && value1 == 0 || 
 	                			value2 == 0 && value1 == 1 ) {
